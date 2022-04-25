67759224213aa6aa9098c4c79cf9ab13e30ff840
alumno <alumno@ubuntu>
2022-04-25 10:20:00 -0700

    PersonasAutorizadas EJB e interfaz

--- BuzzerBeaters/BuzzerBeaters-ejb/src/main/java/negocioEjb/GestionPersonasAutorizadas.java
+++ BuzzerBeaters/BuzzerBeaters-ejb/src/main/java/negocioEjb/GestionPersonasAutorizadas.java
@@ -2,8 +2,12 @@ package negocioEjb;
 
 import java.sql.Date;
 
+import es.uma.BuzzerBeaters.Autorizacion;
+import es.uma.BuzzerBeaters.Cliente;
 import es.uma.BuzzerBeaters.Empresa;
+import es.uma.BuzzerBeaters.PersonaAutorizada;
 import es.uma.BuzzerBeaters.Usuario;
+import negocioEJBexcepcion.UsuarioException;
 
 public interface GestionPersonasAutorizadas {
 	
@@ -13,12 +17,27 @@ public interface GestionPersonasAutorizadas {
 	 * Si el usuario que se intenta introducir ya existe en la base de datos
 	 * debe lanzar una excepcion adecuada.
 	 * user, password y administrador no pueden ser nulos.
+	 * @throws UsuarioException 
 	 **/
 	
 
 
-	void insertarPersonaAutorizada(Usuario user, String identificacion, String nombre, String apellidos, Boolean estado,
-			Date fechaNacimiento, Date fechaInicio, Date fechaFin, Empresa empresa);
+	void insertarPersonaAutorizada(Cliente cliente, String identificacion, String nombre, String apellidos, Boolean estado,
+			Date fechaNacimiento, Date fechaInicio, Date fechaFin, Empresa empresa) throws UsuarioException;
 	
+	/**
+	 * Este metodo permitira modificar los datos de una persona autorizada
+	 * @throws UsuarioException 
+	 */
+	
+	void modificarPersonaAutorizada(PersonaAutorizada persAut, String identificacion, String nombre, String apellidos, Boolean estado,
+			Date fechaNacimiento, Date fechaInicio, Date fechaFin) throws UsuarioException;
+	
+	
+	/**
+	 * Este metodo permitira la eliminacion de autorizados de una cuenta.
+	 */
+	
+	void eliminarAutorizadoEmpresa(PersonaAutorizada persAut, Autorizacion autorizacion)throws UsuarioException;;
 
 }
--- BuzzerBeaters/BuzzerBeaters-ejb/src/main/java/negocioEjb/PersonasAutorizadasEJB.java
+++ BuzzerBeaters/BuzzerBeaters-ejb/src/main/java/negocioEjb/PersonasAutorizadasEJB.java
@@ -1,13 +1,19 @@
 package negocioEjb;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import es.uma.BuzzerBeaters.Autorizacion;
+import es.uma.BuzzerBeaters.Cliente;
 import es.uma.BuzzerBeaters.Empresa;
+import es.uma.BuzzerBeaters.PersonaAutorizada;
 import es.uma.BuzzerBeaters.Usuario;
+import negocioEJBexcepcion.UsuarioException;
 
 public class PersonasAutorizadasEJB implements GestionPersonasAutorizadas{
 	
@@ -18,8 +24,74 @@ public class PersonasAutorizadasEJB implements GestionPersonasAutorizadas{
 
 
 	@Override
-	public void insertarPersonaAutorizada(Usuario user, String identificacion, String nombre, String apellidos,
-			Boolean estado, Date fechaNacimiento, Date fechaInicio, Date fechaFin, Empresa empresa) {		
+	public void insertarPersonaAutorizada(Cliente cliente, String identificacion, String nombre, String apellidos,
+		Boolean estado, Date fechaNacimiento, Date fechaInicio, Date fechaFin, Empresa empresa) throws UsuarioException {		
+		PersonaAutorizada personaAutorizada = new PersonaAutorizada();
+		personaAutorizada.setApellidos(apellidos);
+		personaAutorizada.setFecha_nacimiento(fechaNacimiento);
+		personaAutorizada.setNombre(nombre);
+		personaAutorizada.setIdentification(identificacion);
+		personaAutorizada.setEstado(estado);
+		personaAutorizada.setFechaInicio(fechaInicio);
+		personaAutorizada.setFechaFin(fechaFin);
+		Autorizacion aut = new Autorizacion();
+		
+		aut.setEmpresa(empresa);
+		aut.setPersonaAutorizada(personaAutorizada);
+
+		
+		PersonaAutorizada PerAutEntity = em.find(PersonaAutorizada.class, personaAutorizada);
+		if(PerAutEntity != null) {
+			List <Autorizacion> autorizaciones = PerAutEntity.getAutorizacion();
+			if(!autorizaciones.contains(aut)) {
+				autorizaciones.add(aut);
+				em.persist(PerAutEntity);
+			}else {
+				throw new UsuarioException("El cliente ya tiene la autorizacion que se intenta añadir\n");
+			}
+			
+		}else {
+			List <Autorizacion> autorizaciones = new ArrayList <>();
+			autorizaciones.add(aut);
+			personaAutorizada.setAutorizacion(autorizaciones);
+			em.persist(personaAutorizada);
+
+		}
+		
+	}
+
+
+	@Override
+	public void modificarPersonaAutorizada(PersonaAutorizada persAut, String identificacion, String nombre, String apellidos,
+			Boolean estado, Date fechaNacimiento, Date fechaInicio, Date fechaFin) throws UsuarioException {
+		// TODO Auto-generated method stub
+		PersonaAutorizada PerAutEntity = em.find(PersonaAutorizada.class, persAut);
+		if(persAut == null) {
+			throw new UsuarioException("La persona autorizada no existe en la base de datos");
+		}else {
+			persAut.setApellidos(apellidos);
+			persAut.setEstado(estado);
+			persAut.setFecha_nacimiento(fechaNacimiento);
+			persAut.setFechaFin(fechaFin);
+			persAut.setFechaInicio(fechaInicio);
+		}
+		
+	}
+	
+
+	@Override
+	public void eliminarAutorizadoEmpresa(PersonaAutorizada persAut, Autorizacion autorizacion) throws UsuarioException {
+		PersonaAutorizada PerAutEntity = em.find(PersonaAutorizada.class, persAut);
+		if(persAut == null) {
+			throw new UsuarioException("La persona autorizada no existe en la base de datos");
+		}else {
+			if(PerAutEntity.getAutorizacion().contains(autorizacion)) {
+				PerAutEntity.getAutorizacion().remove(autorizacion);
+			}else {
+				throw new UsuarioException("La autorizacion que se pretende eliminar no existe en la persona autorizada");
+			}
+		}
+		
 		// TODO Auto-generated method stub
 		
 	}
