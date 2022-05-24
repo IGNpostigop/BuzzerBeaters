6219a3ec302b7b6ad8bdc71c8ccaa4191903463f
Nacho <Nacho@DESKTOP-OBD3EII.mshome.net>
2022-05-24 20:32:47 +0200

    personasAutorizadasEJB funciona o al menos no da errores

--- BuzzerBeaters/BuzzerBeaters-ejb/src/main/java/negocioEjb/PersonasAutorizadasEJB.java
+++ BuzzerBeaters/BuzzerBeaters-ejb/src/main/java/negocioEjb/PersonasAutorizadasEJB.java
@@ -61,7 +61,6 @@ public class PersonasAutorizadasEJB implements GestionPersonasAutorizadas{
 			aut.setPersonaAutorizada(pa);
 			em.persist(aut);
 			autorizaciones.add(aut);
-
 		}
 
 		em.merge(emp);
