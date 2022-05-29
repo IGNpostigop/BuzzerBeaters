b9fc5dc34383b6a992318860259f65ef643c5c92
CGPerezAranda <cg.perezaranda@uma.es>
2022-05-29 20:49:29 +0200

    tests altas cuentas

--- BuzzerBeaters/BuzzerBeaters-tests/src/test/java/es/uma/informatica/sii/buzzerbeaters/test/BuzzerBeatersIT.java
+++ BuzzerBeaters/BuzzerBeaters-tests/src/test/java/es/uma/informatica/sii/buzzerbeaters/test/BuzzerBeatersIT.java
@@ -108,4 +108,95 @@ public class BuzzerBeatersIT {
     driver.findElement(By.id("altaCEmpresa")).click();
     assertThat(driver.findElement(By.id("altaCEmpresa:botonEmpresa")).getText(), is("Empresa creada correctamente"));
   }
+  @Test
+  public void altaPA() {
+    driver.get("http://localhost:8080/BuzzerBeaters-war/");
+    driver.manage().window().setSize(new Dimension(876, 1040));
+    driver.findElement(By.linkText("Login para administradores")).click();
+    driver.findElement(By.id("adminLogin:user")).click();
+    driver.findElement(By.id("adminLogin:user")).sendKeys("Fulanito");
+    driver.findElement(By.id("adminLogin:pass")).sendKeys("pass");
+    driver.findElement(By.id("adminLogin:botonLogin")).click();
+    driver.findElement(By.id("botonAltaPersAut")).click();
+    driver.findElement(By.id("altaPersonaAutorizada:username")).click();
+    driver.findElement(By.id("altaPersonaAutorizada:username")).sendKeys("Lolo");
+    driver.findElement(By.id("altaPersonaAutorizada:pass")).click();
+    driver.findElement(By.id("altaPersonaAutorizada:pass")).sendKeys("pass");
+    driver.findElement(By.id("altaPersonaAutorizada:nombre")).sendKeys("Lolo");
+    driver.findElement(By.id("altaPersonaAutorizada:apellido")).sendKeys("Loloa");
+    driver.findElement(By.id("altaPersonaAutorizada:identificacion")).sendKeys("22222p");
+    driver.findElement(By.id("altaPersonaAutorizada:direccion")).sendKeys("grillo");
+    driver.findElement(By.id("altaPersonaAutorizada:fn")).sendKeys("2000-06-23");
+    driver.findElement(By.id("altaPersonaAutorizada:botonPA")).click();
+    driver.findElement(By.id("altaPersonaAutorizada")).click();
+    assertThat(driver.findElement(By.id("altaPersonaAutorizada:boton")).getText(), is("Alta correccta"));
+  }
+  @Test
+  public void anyadirAutorizacion() {
+    driver.get("http://localhost:8080/BuzzerBeaters-war/");
+    driver.manage().window().setSize(new Dimension(876, 1040));
+    driver.findElement(By.linkText("Login para administradores")).click();
+    driver.findElement(By.id("adminLogin:user")).click();
+    driver.findElement(By.id("adminLogin:user")).sendKeys("Fulanito");
+    driver.findElement(By.id("adminLogin:pass")).sendKeys("pass");
+    driver.findElement(By.id("adminLogin:botonLogin")).click();
+    driver.findElement(By.id("botonAnyadirAut")).click();
+    driver.findElement(By.id("anyadirAutorizacion:idPersAut")).click();
+    driver.findElement(By.id("anyadirAutorizacion:idPersAut")).sendKeys("2");
+    driver.findElement(By.id("anyadirAutorizacion:idEmpresa")).click();
+    driver.findElement(By.id("anyadirAutorizacion:idEmpresa")).sendKeys("3");
+    driver.findElement(By.id("anyadirAutorizacion:tipoAutorizacion")).click();
+    driver.findElement(By.id("anyadirAutorizacion:tipoAutorizacion")).sendKeys("a");
+    driver.findElement(By.id("anyadirAutorizacion:botonanyadirAutorizacion")).click();
+    driver.findElement(By.id("anyadirAutorizacion")).click();
+    assertThat(driver.findElement(By.id("anyadirAutorizacion:botonAut")).getText(), is("Alta Correcta"));
+  }
+  @Test
+  public void abreCuentaSegregada() {
+    driver.get("http://localhost:8080/BuzzerBeaters-war/");
+    driver.manage().window().setSize(new Dimension(876, 1040));
+    driver.findElement(By.linkText("Login para administradores")).click();
+    driver.findElement(By.id("adminLogin:user")).click();
+    driver.findElement(By.id("adminLogin:user")).sendKeys("Fulanito");
+    driver.findElement(By.id("adminLogin:pass")).sendKeys("pass");
+    driver.findElement(By.id("adminLogin:botonLogin")).click();
+    driver.findElement(By.id("abreSegregada")).click();
+    driver.findElement(By.id("abreSegregada:clienteid")).click();
+    driver.findElement(By.id("abreSegregada:clienteid")).sendKeys("1");
+    driver.findElement(By.id("abreSegregada:cliente")).click();
+    driver.findElement(By.id("abreSegregada:cliente")).sendKeys("11111111F");
+    driver.findElement(By.id("abreSegregada:iban")).click();
+    driver.findElement(By.id("abreSegregada:iban")).sendKeys("es2021030128310040002352");
+    driver.findElement(By.id("abreSegregada:swift")).click();
+    driver.findElement(By.id("abreSegregada:swift")).sendKeys("unicajaes");
+    driver.findElement(By.id("abreSegregada:comision")).click();
+    driver.findElement(By.id("abreSegregada:comision")).sendKeys("12");
+    driver.findElement(By.id("abreSegregada:ibanReferencia")).click();
+    driver.findElement(By.id("abreSegregada:ibanReferencia")).sendKeys("iban1234");
+    driver.findElement(By.id("abreSegregada:boton")).click();
+    driver.findElement(By.id("abreSegregada:usuario")).click();
+    assertThat(driver.findElement(By.id("abreSegregada:usuario")).getText(), is("Cuenta segregada abierta correctamente."));
+  }
+  
+  @Test
+  public void abreCuentaPooled() {
+    driver.get("http://localhost:8080/BuzzerBeaters-war/");
+    driver.manage().window().setSize(new Dimension(876, 1040));
+    driver.findElement(By.linkText("Login para administradores")).click();
+    driver.findElement(By.id("adminLogin:user")).click();
+    driver.findElement(By.id("adminLogin:user")).sendKeys("Fulanito");
+    driver.findElement(By.id("adminLogin:pass")).click();
+    driver.findElement(By.id("adminLogin:pass")).sendKeys("pass");
+    driver.findElement(By.id("adminLogin:botonLogin")).click();
+    driver.findElement(By.id("abrePooled")).click();
+    driver.findElement(By.id("abrirCuentaPooled:idcliente")).click();
+    driver.findElement(By.id("abrirCuentaPooled:idcliente")).sendKeys("1");
+    driver.findElement(By.id("abrirCuentaPooled:cliente")).sendKeys("11111111F");
+    driver.findElement(By.id("abrirCuentaPooled:iban")).sendKeys("iban1234");
+    driver.findElement(By.id("abrirCuentaPooled:swift")).sendKeys("este");
+    driver.findElement(By.id("abrirCuentaPooled:comision")).sendKeys("12");
+    driver.findElement(By.id("abrirCuentaPooled:boton")).click();
+    driver.findElement(By.id("abrirCuentaPooled:usuario")).click();
+    assertThat(driver.findElement(By.id("abrirCuentaPooled:usuario")).getText(), is("Cuenta pooled abierta correctamente."));
+  }
 }
